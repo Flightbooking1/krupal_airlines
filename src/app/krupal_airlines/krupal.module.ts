a2c8a4cc7a892f932dfe01114e94b48697391d03
AKASH H N <115541234+AkashHN@users.noreply.github.com>
2023-06-30 15:55:56 +0530

    caurosal , footer and header updated (#6)

--- src/app/krupal_airlines/krupal.module.ts
+++ src/app/krupal_airlines/krupal.module.ts
@@ -3,26 +3,35 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { LoginComponent } from './login/login/login.component';
-import {MatTableModule} from '@angular/material/table';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatTabsModule} from '@angular/material/tabs'
-import {MatCheckboxModule} from '@angular/material/checkbox'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTabsModule } from '@angular/material/tabs'
+import { MatCheckboxModule } from '@angular/material/checkbox'
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SearchComponent } from './search/search.component';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { KrupalComponent } from './krupal/krupal.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  imports: [FormsModule,MatFormFieldModule,FormsModule,ReactiveFormsModule,BrowserModule,
+  imports: [FormsModule, MatFormFieldModule, FormsModule, ReactiveFormsModule, BrowserModule,
     MatCardModule,
     MatIconModule,
     MatTabsModule,
@@ -30,7 +39,6 @@ import { BrowserModule } from '@angular/platform-browser';
     MatTableModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatIconModule,
     MatSlideToggleModule,
     MatInputModule,
     MatButtonModule,
@@ -43,10 +51,16 @@ import { BrowserModule } from '@angular/platform-browser';
     MatStepperModule,
     MatDialogModule,
     MatProgressSpinnerModule,
+    MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule,
+    MatAutocompleteModule,
+    MatTooltipModule,
+    MatBadgeModule,
+    BrowserAnimationsModule,
+    // DropdownModule
 
   ],
-  exports: [LoginComponent],
-  declarations: [LoginComponent],
+  exports: [LoginComponent, SearchComponent, KrupalComponent,FooterComponent,HeaderComponent],
+  declarations: [LoginComponent, SearchComponent, KrupalComponent, FooterComponent, HeaderComponent,HeaderComponent],
   providers: [],
 })
 export class KrupalModule { }
